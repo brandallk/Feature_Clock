df1a1f25f92d2143325d77797456d2ac611e35d1
Benjamen R King <brk.3rdof3kings@gmail.com>
2017-02-15 00:30:25 -0700

    Add a module clockTime.js to track and report clock time. Only write pseudocode so far.

---
+++ js/clockTime.js
@@ -0,0 +1,38 @@
+// A module to track and report clock time.
+
+const clockTime = function() {
+  let clockTime = 0;
+  let formattedClockTime = {
+    hours: 12,
+    minutes: 00,
+    seconds: 00
+  };
+
+  // Update clockTime variable by +1sec (forward direction) or -1sec (reverse
+  // direction) for each elapsed tickInterval.
+  function timer(increment) {
+    // Add increment (either +1sec or -1sec) to variable clockTime
+  }
+  function startTimer(direction, tickInterval) {
+    // If direction=forward, call timer(+1sec) every tickInterval
+    // If direction=reverse, call timer(-1sec) every tickInterval
+  }
+  function stopTimer() {
+    // clearInterval()
+  }
+  function formatTime() {
+    // Convert current (unformatted) clockTime integer into formattedClockTime
+    return formattedClockTime;
+  }
+  function getClockTime() {
+    formatTime();
+    return formattedClockTime;
+  }
+
+  return {
+    startTimer : startTimer, // Call this with direction, tickInterval when clock starts running
+    stopTimer : stopTimer, // Call this when clock stops running (Pause clockTime)
+    getClockTime : getClockTime // Call this every tick to update digital display 
+  };
+
+}();
